0edb728afd188d64ad26af11c2c5e9ddaef9f1fe
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-06 18:39:54 +0100

    Clarified no comment instructions with logged in user detection.

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -14,7 +14,15 @@
 	
 		<h3>Comments Plz!</h3>
 		
-		<p>We don't yet have any comments on this.</p>
+		<?php
+		
+		if ($this->tweet->logged_in()){
+			echo '<p>We don\'t yet have any comments on this page. To leave yours just click on the paragraph which sparked your thoughts.</p>';
+		}else{
+			echo '<p>We don\'t yet have any comments on this page. To leave yours you\'ll first need to <a href="' . site_url('signin') . '">sign in</a>.</p>';
+		}
+		
+		?>
 	
 	</div>
 			
