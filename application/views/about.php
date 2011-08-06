2edf7db84768b2d558555ac6fd9c35e6a1742d15
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-06 16:00:34 +0100

    Dropped Digress.it source into the tree so it's easier to find.

---
+++ application/views/about.php
@@ -0,0 +1,28 @@
+<div class="grid_8"> 
+		
+	<h1>About Us</h1>
+	
+	<h2>How The Magic Happens</h2>
+	
+	<ol>
+		<li>You tell us which web page you want us to make commentable.</li>
+		<li>We grab the contents of the web page and run it through some algorithms to extract just the content, and strip out things like navigation.</li>
+		<li>We store the page contents and display them through our own commenting interface.</li>
+		<li>You can leave your thoughts on individual paragraphs, much like scribbling in the margin of a book or newspaper article.</li>
+	</ol>
+	
+</div> 
+
+<aside class="grid_4 last"> 
+	
+	<div class="box bg_light"> 
+	
+		<h3>Digress.it</h3>
+		
+		<p>Feel The Vibe's commenting system is built on <a href="http://digress.it/">Digress.it</a>, a plugin for <a href="http://wordpress.org/">WordPress</a> which lets you achieve the same effect on your own blog.</p>
+		
+		<p>Of course, you can just keep using us.</p>
+			
+	</div>
+			
+</aside>
