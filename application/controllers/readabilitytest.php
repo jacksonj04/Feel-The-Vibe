2271502f0baf72c1e6e0b1a4950f9163cdae9067
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-06 14:47:13 +0100

    Debug

--- application/controllers/readabilitytest.php
+++ application/controllers/readabilitytest.php
@@ -16,7 +16,8 @@ class Readabilitytest extends CI_Controller {
 		$url = 'http://www.bbc.co.uk/news/world-us-canada-14428930';
 		
 		if ($this->parser->url($url))
-		{			
+		{
+			die(var_dump($this->parser->title));
 			if ($p = $this->post->add($this->parser->title, $this->parser->content))
 			{
 				echo $p;	
