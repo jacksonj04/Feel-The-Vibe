4056ed5b9b43ad868ee8500bd55a9cfe3a0e1ac7
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-07 00:49:23 +0100

    Added ajax/vibeup, ajax/vibedown and ajax/unvibe.

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -69,4 +69,72 @@ class Ajax extends CI_Controller {
 		}
 		
 	}
+	
+	public function vibedown()
+	{
+	
+		if ($this->input->post('post') && $this->input->post('paragraph')){
+			// We have post and paragraph. Validation time!
+			$post_db = $this->db->where('post_id', $this->input->post('post'))->get('posts');
+			if ($post_db->num_rows() == 1){
+				// Post exists, be happy
+				$user = $this->user->getcurrent();
+				$vibes_db = $this->db->where('post_id', $this->input->post('post'))->where('paragraph', $this->input->post('paragraph'))->where('user_id', $user->user_id)->get('vibes');
+				// Test for existing vibes
+				if ($vibes_db->num_rows() == 1){
+					// Vibe exists. Update.
+					$vibe = array(
+						'vibe'		=>	'down',
+						'timestamp'	=>	time()
+					);
+					
+					$this->db->where('post_id', $this->input->post('post'))->where('paragraph', $this->input->post('paragraph'))->where('user_id', $user->user_id)->update('vibes', $vibe);
+					
+				}else{
+					// No vibe. Put one in.
+					$vibe = array(
+						'user_id'	=>	$this->user->getcurrent()->user_id,
+						'post_id'	=>	$this->input->post('post'),
+						'paragraph'	=>	$this->input->post('paragraph'),
+						'vibe'		=>	'down',
+						'timestamp'	=>	time()
+					);
+					
+					$this->db->insert('vibes', $vibe);
+				}
+				
+				echo json_encode(array('message' => 'Vibe down logged!'));
+				
+			}else{
+				// Post doesn't exist, something has gone wrong.
+				echo json_encode(array('error' => 'Post does not exist.'));
+			}
+		}else{
+			echo json_encode(array('error'=>'Unable to complete vibe down.'));
+		}
+		
+	}
+	
+	public function unvibe()
+	{
+	
+		if ($this->input->post('post') && $this->input->post('paragraph')){
+			// We have post and paragraph. Validation time!
+			$post_db = $this->db->where('post_id', $this->input->post('post'))->get('posts');
+			if ($post_db->num_rows() == 1){
+				// Post exists, be happy
+				$user = $this->user->getcurrent();
+				$vibes_db = $this->db->where('post_id', $this->input->post('post'))->where('paragraph', $this->input->post('paragraph'))->where('user_id', $user->user_id)->delete('vibes');
+				
+				echo json_encode(array('message' => 'Paragraph unvibed!'));
+				
+			}else{
+				// Post doesn't exist, something has gone wrong.
+				echo json_encode(array('error' => 'Post does not exist.'));
+			}
+		}else{
+			echo json_encode(array('error'=>'Unable to complete vibe down.'));
+		}
+		
+	}
 }
