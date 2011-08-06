040a8a4e494a28efeba709a1df93e1d95f89ae23
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-07 00:28:29 +0100

    Bug fix

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function hideHeatMap(e)
 {
 	e.preventDefault();
 	
-	$('.vibe3,.vibe2,.vibe1,.vibe-1,.vibe-2,.vibe-3').removeClass('vibe3 vibe2 vibe1 vibe-1 vibe-2 vibe-3');
+	$('.vibe3,.vibe2,.vibe1,.vibe-1,.vibe-2,.vibe-3,.vibe0').removeClass('vibe0 vibe3 vibe2 vibe1 vibe-1 vibe-2 vibe-3');
 }
 
 $(function(){
