9cd8f5d2febfec5ed6c62e26d2a45767116e2a39
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-06 13:01:12 +0100

    Update

--- application/controllers/readabilitytest.php
+++ application/controllers/readabilitytest.php
@@ -10,6 +10,12 @@ class Readabilitytest extends CI_Controller {
 	{
 		$this->load->library('readability');
 		
+		$url = 'http://www.bbc.co.uk/news/world-us-canada-14428930';
+		$html = file_get_contents($url);
+		
+		$raw = $this->readability->get($html);
+		
+		echo $raw;
 		
 	}
 
