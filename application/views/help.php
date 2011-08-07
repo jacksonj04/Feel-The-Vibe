64465fcfe45e939c3f8cb3bd5e90f7be6b5116e0
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-07 07:52:53 +0100

    More help text.

--- application/views/help.php
+++ application/views/help.php
@@ -4,7 +4,19 @@
 	
 	<h2>Creating</h2>
 	
+	<p>To create a new commentable document it's easy. You can either paste the URL of the page into our <a href="<?php echo site_url('create'); ?>">create form</a>, or add our bookmarklet (at the bottom of the create page) to your bookmarks bar and enjoy one-click creation!</p>
+	
+	<h3>Something has gone horrifically wrong! My document looks like it's been violently attacked by a typesetter on an acid trip!</h3>
+	
+	<p>It happens sometimes. We're still fine-tuning our algorithms to make sure we extract the page as correctly as possible. Drop a tweet to <a href="http://twitter.com/alexbilbie">@alexbilbie</a> or  <a href="http://twitter.com/jacksonj04">@jacksonj04</a> with details and we'll have a look at what we can do.</p>
+	
 	<h2>Commenting</h2>
+	
+	<p>Feel The Vibe lets you comment on each paragraph individually, much like scribbling in the margins of a book or paper. This means you can see comments in their context, with no need to scroll backwards and forwards between a comments section and the document you're interested in.</p>
+	
+	<h3>How can I add a comment?</h3>
+	
+	<p>To comment on a paragraph just make sure you're signed in, then click the paragraph you want to comment on. You'll be shown any existing comments for that paragraph and given the option to write your own.</p>
 	
 	<h2>Vibes</h2>
 	
@@ -16,7 +28,7 @@
 	
 	<p>Make sure you're signed in, then hover over a paragraph. You'll see the thumb up and thumb down icons appear; just click the one you want. You'll see a red or green line appear down the right hand side of the paragraph (depending on which thumb you chose) and the appropriate thumb icon will become permanently visible.</p>
 	
-	<p>How do I see other people's vibes?</p>
+	<h3>How do I see other people's vibes?</h3>
 	
 	<p>We'll automatically display vibes by highlighting paragraphs in the appropriate colour and by showing a 'vibe total' next to them. You can toggle the display of vibes on or off using the option in the commenting box.</p>
 	
