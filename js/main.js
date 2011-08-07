eaa967d70b33ba6d0d3ea2a5589bb99ba07d66e3
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-07 01:05:35 +0100

    vibeUp function

--- js/main.js
+++ js/main.js
@@ -18,8 +18,28 @@ function hideHeatMap(e)
 	$('.vibe3,.vibe2,.vibe1,.vibe-1,.vibe-2,.vibe-3,.vibe0').removeClass('vibe0 vibe3 vibe2 vibe1 vibe-1 vibe-2 vibe-3');
 }
 
+function vibeUp(e)
+{
+	e.preventDefault();
+	
+	var para = $(this).parents().find('.para').attr('id');
+	para = para.replace('para_');
+	
+	var postid = $('#viewer').data('postid');
+	
+	$.ajax({
+		type:	'POST',
+		url:	'/ajax/vibeup',
+		data:	'post='+postid+'&paragraph='+para,
+		dataType: 'json'
+	});
+}
+
 $(function(){
 
 	$('#toggleHeatMap').toggle(hideHeatMap, showHeatMap);
+	
+	$('.vibe-up').bind('click', vibeUp);
+	$('.vibe-down').bind('click', vibeDown);
 
 });
