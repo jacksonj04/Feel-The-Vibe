19fc7f83c7b0619af3763d1ed336a2af0420979f
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-07 04:43:46 +0100

    Enabled Vibe Down

--- js/main.js
+++ js/main.js
@@ -28,11 +28,27 @@ function vibeUp(e)
 	});
 }
 
+function vibeDown(e)
+{
+	e.preventDefault();
+	
+	var para = $(this).data('paraid');
+	
+	var postid = $('#viewer').data('postid');
+	
+	$.ajax({
+		type:	'POST',
+		url:	'/ajax/vibedown',
+		data:	'post='+postid+'&paragraph='+para,
+		dataType: 'json'
+	});
+}
+
 $(function(){
 
 	$('#toggleHeatMap').toggle(hideHeatMap, showHeatMap);
 	
 	$('.vibe-up').bind('click', vibeUp);
-	//$('.vibe-down').bind('click', vibeDown);
+	$('.vibe-down').bind('click', vibeDown);
 
 });
