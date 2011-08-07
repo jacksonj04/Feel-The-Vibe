50b8738faabf15e8d6cca3716f942a9163a6d5d2
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-07 11:27:46 +0100

    Test multi submit

--- application/controllers/post_view.php
+++ application/controllers/post_view.php
@@ -112,6 +112,48 @@ class Post_view extends CI_Controller {
 		
 		$head = array('currentpage' => 'create');
 		
+		if ($this->input->post('create'))
+		{
+		
+			if ($user = $this->user->getcurrent())
+			{
+		
+				$urls = $this->input->post('multiurl[]');
+				
+				die(var_dump($urls));
+				
+				$posts_db = $this->db->where('original_url', $url)->get('posts');
+				
+				
+				$this->load->model('parser');
+				
+				if ($this->parser->url($url))
+				{
+					if ($p = $this->post->add($url, $this->parser->title, $this->parser->content, NULL, 1, $user->user_id))
+					{
+						$data['success'] = $p;
+					}
+					
+					else
+					{
+						$data['error'] = $this->parser->error_message;
+					}
+				}
+				
+				else
+				{
+					$data['error'] = $this->parser->error_message;
+				}
+					
+
+			}
+			
+			else
+			{
+				$data['error'] = 'You\'re not signed in!';
+			}
+		}
+		
 		$this->load->view('inc/head', $head);
 		$this->load->view('post/createmulti', $data);
 		$this->load->view('inc/foot');
