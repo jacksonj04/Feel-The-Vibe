ad32a7025092931e18f4c9a5622f6b78adf52b87
Nick Jackson <nijackson@lincoln.ac.uk>
2011-08-07 06:17:29 +0100

    Assorted stuff and some CSS

--- application/views/help.php
+++ application/views/help.php
@@ -2,20 +2,22 @@
 		
 	<h1>Help</h1>
 	
-	<h2>Do I really need Twitter to sign in?</h2>
+	<h2>Creating</h2>
 	
-	<p>Yes.</p>
+	<h2>Commenting</h2>
 	
-	<h2>How does it work?</h2>
+	<h2>Vibes</h2>
 	
-	<p>Voodoo.</p>
+	<h3>What is a vibe?</h3>
 	
-	<h2>Can I really use any web page?</h2>
+	<p>A vibe is a simple 'thumbs up' or 'thumbs down' against a specific paragraph which you can use to express your approval or disapproval of what's being said.</p>
 	
-	<p>Yes. We can't promise that we'll be able to turn it in to a decent quality commentable document and we tend to do far better with pages which contain an article, but you can give us any page and we'll do our best.</p>
+	<h3>How can I leave a vibe?</h3>
 	
-	<h2>What are the thumb up and thumb down links for?</h2>
+	<p>Make sure you're signed in, then hover over a paragraph. You'll see the thumb up and thumb down icons appear; just click the one you want. You'll see a red or green line appear down the right hand side of the paragraph (depending on which thumb you chose) and the appropriate thumb icon will become permanently visible.</p>
 	
-	<p>These let you give either a positive or negative 'vibe' to a specific paragraph. If you like what it says, give it the thumbs up. If you don't, thumb it down.</p>
+	<p>How do I see other people's vibes?</p>
+	
+	<p>We'll automatically display vibes by highlighting paragraphs in the appropriate colour and by showing a 'vibe total' next to them. You can toggle the display of vibes on or off using the option in the commenting box.</p>
 	
 </div>
