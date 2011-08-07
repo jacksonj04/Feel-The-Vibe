dad106d36ae5d97bd7e72a37b8eb22f393add65f
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-07 10:25:43 +0100

    Styling

--- application/views/post/create.php
+++ application/views/post/create.php
@@ -1,4 +1,4 @@
-<div class="grid_12">
+<div class="grid_8 first">
 	
 	<h1>Create a New Page</h1>
 
@@ -90,4 +90,26 @@
 	
 	?>
 
+</div>
+
+<div class="grid_4 last">
+
+	<?php if ($this->user->getcurrent()):	?>
+	
+	<div class="box bg_light">
+		
+		<h3>Want to make it even easier?</h3>
+		
+		<p>Drag our bookmarklet (below) to your bookmarks bar!</p>
+			
+		<p>
+			<a style="padding:5px;background:#666;color:white;border-radius:5px;" href="javascript:(function(){var FTVForm=document.createElement('form');FTVForm.setAttribute('method','post');FTVForm.setAttribute('action','http://feelthevi.be/create');var FTVFieldURL=document.createElement('input');FTVFieldURL.setAttribute('type','hidden');FTVFieldURL.setAttribute('name','url');FTVFieldURL.setAttribute('value',window.location);FTVForm.appendChild(FTVFieldURL);var FTVFieldCreate=document.createElement('input');FTVFieldCreate.setAttribute('type','hidden');FTVFieldCreate.setAttribute('name','create');FTVFieldCreate.setAttribute('value','Create');FTVForm.appendChild(FTVFieldCreate);document.body.appendChild(FTVForm);FTVForm.submit();})()" title="feelthevi.be">Drag Me!</a>
+		</p>
+		
+	</div>
+	
+	<?php
+	endif;
+	?>
+	
 </div>
