152c78544bbe4ff5fe0e4adc27e10b7370fe3cc8
Alex Bilbie <alex.bilbie@gmail.com>
2011-08-07 09:54:56 +0100

    Remove paragraph margin top in #pageinfo

--- application/views/post/index.php
+++ application/views/post/index.php
@@ -1,7 +1,7 @@
 <div class="grid_12">
 	<h1><?php echo $title; ?></h1>
 	
-	<div class="grid_12 first margin_bottom border_bottom padding_bottom">
+	<div class="grid_12 first margin_bottom border_bottom padding_bottom" id="pageinfo">
 		<p class="grid_9">
 			<a href="<?php echo $cap_url; ?>" title="<?php $this->load->helper('text'); echo ellipsize($cap_url,25,0.75); ?>">Original page</a> 
 			submitted by 
